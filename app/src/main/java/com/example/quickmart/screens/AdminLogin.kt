96438322d58c2528841d0ca1bf6431e22279395c
jeff <geoffreyerastus956@gmail.com>
2023-09-14 11:21:44 -0400

    initial

--- app/src/main/java/com/example/quickmart/screens/AdminLogin.kt
+++ app/src/main/java/com/example/quickmart/screens/AdminLogin.kt
@@ -1,12 +1,20 @@
 package com.example.quickmart.screens
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.quickmart.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.quickmart.databinding.ActivityAdminLoginBinding
 
 class AdminLogin : AppCompatActivity() {
+    private lateinit var binding: ActivityAdminLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityAdminLoginBinding.inflate(layoutInflater)
+        supportActionBar?.hide()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_admin_login)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this, AdminUploads::class.java))
+        }
     }
 }
